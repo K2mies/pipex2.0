ea763faf54dbbab2cc230073282fcde63471356e
Ross Hvidsten <rhvidste@student.hive.fi>
2025-01-20 10:15:17 +0200

    submition of corrected pipex for evaluation

---
+++ src/pipex.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/10 19:08:51 by rhvidste          #+#    #+#             */
+/*   Updated: 2025/01/17 17:58:51 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	child_1_process(char **argv, char **envp, int *fd)
+{
+	int	file1;
+
+	file1 = open(argv[1], O_RDONLY, 0777);
+	if (file1 == -1)
+		ft_error(argv[1], true);
+	dup2(file1, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close(fd[0]);
+	cmd_exec(argv[2], envp);
+}
+
+void	child_2_process(char **argv, char **envp, int *fd)
+{
+	int	file2;
+
+	file2 = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0777);
+	if (file2 == -1)
+		ft_error(argv[4], true);
+	dup2(fd[0], STDIN_FILENO);
+	dup2(file2, STDOUT_FILENO);
+	close(fd[1]);
+	cmd_exec(argv[3], envp);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		fd[2];
+	pid_t	child1;
+	pid_t	child2;
+
+	if (argc == 5)
+	{
+		if (pipe(fd) == -1)
+			ft_error(argv[0], false);
+		child1 = fork();
+		if (child1 == -1)
+			ft_error(argv[0], false);
+		if (child1 == 0)
+			child_1_process(argv, envp, fd);
+		child2 = fork();
+		if (child2 == -1)
+			ft_error(argv[0], false);
+		if (child2 == 0)
+			child_2_process(argv, envp, fd);
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(child1, NULL, 0);
+		waitpid(child2, NULL, 0);
+	}
+	else
+		write(2, "wrong number of variables\n", 27);
+}
---
+++ src/utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 10:02:10 by rhvidste          #+#    #+#             */
+/*   Updated: 2025/01/17 17:58:47 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pipex.h>
+
+void	ft_error(char *argv, bool flag)
+{
+	if (flag == true)
+	{
+		write(2, "pipex: ", 8);
+		perror(argv);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		perror("error");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	free_array(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
+}
+
+char	*path_parsing(char *command, char **envp)
+{
+	char	**mypaths;
+	char	*onepath;
+	char	*fullpath;
+	int		i;
+
+	i = 0;
+	while (!ft_strnstr(envp[i], "PATH", 4))
+		i++;
+	mypaths = ft_split(envp[i] + 5, ':');
+	i = 0;
+	while (mypaths[i])
+	{
+		onepath = ft_strjoin(mypaths[i], "/");
+		fullpath = ft_strjoin(onepath, command);
+		free(onepath);
+		if (access(fullpath, F_OK) == 0)
+			return (fullpath);
+		i++;
+	}
+	free_array(mypaths);
+	return (0);
+}
+
+void	cmd_exec(char *argv, char **envp)
+{
+	char	**command;
+	char	*path;
+
+	command = ft_split(argv, ' ');
+	if (ft_strchr(command[0], '/'))
+		path = command[0];
+	else
+		path = path_parsing(command[0], envp);
+	if (execve(path, command, envp) == -1)
+	{
+		free_array(command);
+		ft_error(argv, true);
+	}
+}
